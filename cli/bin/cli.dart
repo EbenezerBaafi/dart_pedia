d17101f9f36754ae2d9495fbb1ee97628db8cb58
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-03-27 04:07:45 +0000

    successfully built the basic search command with user input handling, correctly treating multi-word command-line inputs as a single search phrase in the output

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,3 +1,39 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+const version = '0.0.1';
 void main(List<String> arguments) {
-  print('Hello dart!:');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI Version: $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // if the user didn't pass in an article title, prompt them to enter one
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title');
+
+    // await input and provide a default value if the user doesn't enter anything
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join('');
+  }
+  print('Looking up articles about article $articleTitle');
+  print('Yey here you go!!');
+  print('Current article title $articleTitle');
+}
+
+void printUsage() {
+  print(
+    'The following commands are valid: "help", "version", "search <artticle title>"',
+  );
 }

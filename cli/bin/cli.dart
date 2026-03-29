271453fe172903f127308809d4a240b86de287fb
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-03-29 23:42:50 +0000

    Created a custom ArgumentException
    feat: Implemented error handling in CommandRunner

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -3,8 +3,16 @@ import 'package:command_runner/command_runner.dart';
 const version = '0.0.1';
 void main(List<String> arguments) {
   // main is now async and awaits the runner
-  var commandRunner = CommandRunner()
-    ..addCommand(HelpCommand()); // Create an instance of your new CommandRunner
+  var commandRunner = CommandRunner(
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand()); // Create an instance of your new CommandRunner
   commandRunner.run(
     arguments,
   ); // Call its run method, awaiting its Future<void>

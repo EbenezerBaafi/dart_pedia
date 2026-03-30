6da4b3d60a6c60db27332a849fc7fbbb186d7c88
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-03-30 00:11:52 +0000

    Debugged an error in the cli.dart file

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,19 +1,17 @@
 import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
-void main(List<String> arguments) {
-  // main is now async and awaits the runner
+Future<void> main(List<String> arguments) async {
   var commandRunner = CommandRunner(
     onError: (Object error) {
-      if (error is Error) {
-        throw error;
-      }
+      if (error is Error) throw error;
+
       if (error is Exception) {
         print(error);
       }
     },
   )..addCommand(HelpCommand()); // Create an instance of your new CommandRunner
-  commandRunner.run(
+  await commandRunner.run(
     arguments,
   ); // Call its run method, awaiting its Future<void>
 }
